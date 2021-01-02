b1a462df4d38bd9cbcb2f23a443b27a6a23989dc
root <root@ip-172-31-40-216.ap-southeast-1.compute.internal>
2021-01-02 09:57:28 +0000

    test version 3

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World 22222');
+  res.send('Hello World 22222, 33333');
 });
 
 app.listen(PORT, HOST);
